51e566478c2823832862fe709ab7daf6f8fce38b
weann2020 <anw2@unlv.nevada.edu>
2020-08-21 17:22:08 +0000

    updated async.py

--- async.py
+++ async.py
@@ -3,12 +3,12 @@ import asyncio
 async def add(a,b):
     c = a+b
     await asyncio.sleep(5)
-    print(f"The sum is: {c}")
+    print(f"The sum is: {c}. First job done.")
 
 async def multiply(a,b):
     c = a *b
     await asyncio.sleep(1)
-    print(f"The product is: {c}")
+    print(f"The product is: {c}. Second job done.")
 
 
 async def main():
@@ -17,7 +17,7 @@ async def main():
     task2 = asyncio.create_task(multiply(3, 5))
     
     await asyncio.gather(task1, task2)
-    print("Done!")
+    print("All jobs done!")
 
 
 if __name__ == "__main__":
